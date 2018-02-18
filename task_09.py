13fedf1c070864cf9ef575cddbf99cca1f785fde
Jamel Travis <jt12057930@IS210-1-1182-25.sps.cuny.edu>
2018-02-18 18:17:28 -0500

    task 10 finished

--- task_09.py
+++ task_09.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""A computer that can build the question."""
+"""Does this count?
+
+A computer that can build the question.
+
+Add a multi-line docstring to task_09.py.
+The docstring should break across two paragraphs.
+If you want to test your docstring, try the following
+commands from the Python interactive command line by using help().
+"""
 
 THE_ANSWER_TO_EVERYTHING = 42
 
--- task_10.py
+++ task_10.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""Task 10"""
+"""Task 10
+
+Use a string function that will change MOVIE to titlecase and
+save its result into a new variable named ENTITLED
+"""
 
 MOVIE = 'dr. strangelove or: how i learned to stop worrying and love the bomb'
+
+ENTITLED = MOVIE.title()
+
+# print ENTITLED
